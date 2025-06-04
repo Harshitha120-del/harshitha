c10c87322df13d5e0178d66a55cad125d3e939f5
Harshitha120-del <harshithachejerla68@gmail.com>
2025-06-04 10:53:57 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main1
+{
+public static void main(String[] args)
+{
+String s="Sachin";
+s.concat("Tendulkar");
+System.out.println(s);
+}
+}
---
+++ Main3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main3
+{
+public static void main(String[] args){
+String name="hello";
+System.out.println("name");
+String newString=new String("hello");
+System.out.println("String newString="+newString);
+}
+}
